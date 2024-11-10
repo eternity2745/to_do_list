602df83006193f9d42e349123aa7c6b344d7e029
eternity2745 <vsdevan2006@gmail.com>
2024-11-10 14:40:00 +0530

    Added Pageview

---
+++ lib/Screens/upcomingTasks.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class UpcomingTasks extends StatefulWidget {
+  const UpcomingTasks({super.key});
+
+  @override
+  State<UpcomingTasks> createState() => _UpcomingTasksState();
+}
+
+class _UpcomingTasksState extends State<UpcomingTasks> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
